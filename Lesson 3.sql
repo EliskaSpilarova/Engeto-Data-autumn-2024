4bfacfa3715d5332ba0080626a766d36e10fe6e6
Eliška Špilarová <164064165+EliskaSpilarova@users.noreply.github.com>
2025-01-19 16:04:38 +0100

    More exercises added

--- Lesson 3.sql
+++ Lesson 3.sql
@@ -46,3 +46,49 @@ SELECT
 FROM czechia_price 
 WHERE region_code = 'CZ064'
 GROUP BY category_code;
+
+-- Úkol 3: Sečtěte průměrné ceny potravin za všechny kategorie, u kterých měření probíhalo od (date_from) 15. 1. 2018.
+SELECT 
+	sum(value) AS sum_of_average_prices
+FROM czechia_price
+WHERE date_from = '2018-01-15';
+
+-- Úkol 4: Vypište tři sloupce z tabulky czechia_price: kód kategorie, počet řádků pro ni a sumu hodnot průměrných cen. 
+-- To vše pouze pro data v roce 2018.
+SELECT 
+	category_code,
+	count(id) AS number_of_rows,
+	sum(value) AS sum_of_average_prices
+FROM czechia_price cp
+WHERE date_part ('year', date_from) = 2018
+GROUP BY category_code;
+
+-- Cvičení: Další agregační funkce
+-- Úkol 1: Vypište maximální hodnotu průměrné mzdy z tabulky czechia_payroll.
+SELECT 
+	max(value)
+FROM czechia_payroll cp 
+WHERE value_type_code = 5958;
+
+-- Úkol 2: Na základě údajů v tabulce czechia_price vyberte pro každou kategorii potravin její minimum v letech 2015 až 2017.
+SELECT 
+	category_code,
+    min(value) AS category_minumum	
+FROM czechia_price
+WHERE date_part('year', date_from) BETWEEN 2015 AND 2017
+GROUP BY category_code;
+
+-- Úkol 3: Vypište kód (případně i název) odvětví s historicky nejvyšší průměrnou mzdou.
+SELECT
+    industry_branch_code
+FROM czechia_payroll
+WHERE value IN (
+    SELECT
+        max(value)
+    FROM czechia_payroll
+    WHERE value_type_code = 5958
+);
+ 
+-- Úkol 4: Pro každou kategorii potravin určete její minimum, maximum a vytvořte nový sloupec s názvem difference, 
+-- ve kterém budou hodnoty "rozdíl do 10 Kč", "rozdíl do 40 Kč" a "rozdíl nad 40 Kč" na základě rozdílu minima a maxima. 
+-- Podle tohoto rozdílu data seřaďte.
